03151496cc330ff5348e7100dcc7a9a67f75cd98
Biswojit Biswal <120778980+biswojitbiswal@users.noreply.github.com>
2024-10-25 11:38:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let btn = document.getElementById("enterBtn");
+let para = document.getElementById("status");
+
+btn.addEventListener("click", function(){
+	para.innerHTML = "Entered Metaverse";
+})
